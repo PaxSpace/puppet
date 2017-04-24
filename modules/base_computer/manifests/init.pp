f7eea0dd327028a09b7b9e869d4c04c39325ea7a
Paul Di Biase <quartermaster@paxspace.org>
2017-04-24 11:24:00 -0400

    added periodic updates

--- modules/base_computer/manifests/init.pp
+++ modules/base_computer/manifests/init.pp
@@ -1,2 +1,10 @@
 class base_computer {
+    exec {"apt-get update" :
+        command => "/usr/bin/apt-get update"
+    }
+    
+    package {"cron" : ensure => "latest"}
+    package {"vim" : ensure => "latest"}
+    package {"git" : ensure => "latest"}
+    package {"sudo" : ensure => "latest"}
 }
--- modules/base_computer/manifests/update_modules.pp
+++ modules/base_computer/manifests/update_modules.pp
@@ -1,2 +1,14 @@
 class base_computer::update_modules {
+    vcsrepo { '/opt/puppet':
+        ensure => latest,
+        provider => git,
+        source => 'https://github.com/PaxSpace/puppet.git',
+        revision => 'master'
+    }
+
+    cron { 'runPuppetUpdates' :
+        ensure => present,
+        command => "/bin/bash /opt/puppet/runAgent.sh",
+        minute => ['0','30']
+    }
 }
