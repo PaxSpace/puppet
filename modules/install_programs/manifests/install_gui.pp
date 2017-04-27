150385f5d807eef2edca87fe5ca8ae3add2891fd
Paul Di Biase <paul.a.dibiase@gmail.com>
2017-04-27 05:11:56 +0000

    Revert "forcing jre package install"
    This reverts commit 9ed9b5fe90544201183976538e53daf5c9a2ff0e.

--- modules/install_programs/manifests/install_gui.pp
+++ modules/install_programs/manifests/install_gui.pp
@@ -3,7 +3,7 @@ class install_programs::install_gui {
     include 'install_programs::install_devtools'
     include 'install_programs::install_design' 
 
-    $apt_packages = ['default_jre','gnome','gdm3']
+    $apt_packages = ['gnome','gdm3']
     
     package { $apt_packages: ensure => latest, require => Exec['apt-get update']}
 }
