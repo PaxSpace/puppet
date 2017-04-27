9fcda5d94e4ad8fc3521f9a0ed602147c8833a8d
Paul Di Biase <paul.a.dibiase@gmail.com>
2017-04-27 04:46:33 +0000

    output agent runs to stdout and log files

--- runAgent.sh
+++ runAgent.sh
@@ -4,4 +4,4 @@ mkdir -p /var/log/puppet_agent
 
 puppet apply --verbose --modulepath=$PWD/modules/ -e "class{'base_computer': }"
 puppet apply --verbose --modulepath=$PWD/modules/ -e "class{'base_computer::update_modules': }"
-puppet apply --verbose --modulepath=$PWD/modules $PWD/manifests > /var/log/puppet_agent/`date +\%Y\%m\%d-\%H\%M`.log 2>&1
+puppet apply --verbose --modulepath=$PWD/modules $PWD/manifests 2>&1 | tee /var/log/puppet_agent/`date +\%Y\%m\%d-\%H\%M`.log 
