23ba2324fa0f8e6db5cf9d568d761411bfaa5e72
Paul Di Biase <paul.a.dibiase@gmail.com>
2017-04-27 18:09:27 +0000

    adding missing directory for java install

--- modules/install_programs/manifests/install_gui.pp
+++ modules/install_programs/manifests/install_gui.pp
@@ -3,6 +3,11 @@ class install_programs::install_gui {
     include 'install_programs::install_devtools'
     include 'install_programs::install_design' 
 
+    file { '/usr/share/man/man1' :
+        ensure => directory,
+        before => Package['gnome']
+    }
+
     $apt_packages = ['gnome','gdm3']
     
     package { $apt_packages: ensure => latest, require => Exec['apt-get update']}
