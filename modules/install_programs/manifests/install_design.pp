6d82ef01403811bc74a7b756c8319a4f46ac60b0
Paul Di Biase <paul.a.dibiase@gmail.com>
2017-04-27 04:55:50 +0000

    removing netfabb from apt install

--- modules/install_programs/manifests/install_design.pp
+++ modules/install_programs/manifests/install_design.pp
@@ -1,5 +1,5 @@
 class install_programs::install_design {
-    $apt_packages = ['blender','inkscape','openscad','netfabb','pinta']
+    $apt_packages = ['blender','inkscape','openscad','pinta']
 
     package { $apt_packages: ensure => latest, require => Exec['apt-get update']}
 }
--- modules/install_programs/manifests/remove_design.pp
+++ modules/install_programs/manifests/remove_design.pp
@@ -1,5 +1,5 @@
 class install_programs::remove_design {
-    $apt_packages = ['blender','inkscape','openscad','netfabb','pinta']
+    $apt_packages = ['blender','inkscape','openscad','pinta']
 
     package { $apt_packages: ensure => absent, require => Exec['apt-get update']}
 }
