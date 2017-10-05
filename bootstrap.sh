61cd58e0ca2351b9d00a912f27fdb844db7ab08b
Paul Di Biase <paul.a.dibiase@gmail.com>
2017-10-05 00:52:28 -0400

    changing cloning in dev to dev branch for first run through

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ cd /opt
 apt-get update
 apt-get install -y puppet git coreutils
 
-git clone https://github.com/PaxSpace/puppet.git
+git clone https://github.com/PaxSpace/puppet.git --branch dev --depth 1
 
 cd puppet
 
